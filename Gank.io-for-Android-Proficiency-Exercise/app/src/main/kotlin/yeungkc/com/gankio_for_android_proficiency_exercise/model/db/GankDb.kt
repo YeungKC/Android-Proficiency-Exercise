c3b424403cf633d108275aea1f0b5044fa6a6905
YeungKC <yeungkcyeungkcyeungkc@gmail.com>
2016-12-07 16:20:10 +0800

    增加 M 层（网络，数据库）测试

--- Gank.io-for-Android-Proficiency-Exercise/app/src/main/kotlin/yeungkc/com/gankio_for_android_proficiency_exercise/model/db/GankDb.kt
+++ Gank.io-for-Android-Proficiency-Exercise/app/src/main/kotlin/yeungkc/com/gankio_for_android_proficiency_exercise/model/db/GankDb.kt
@@ -1,35 +1,40 @@
 package yeungkc.com.gankio_for_android_proficiency_exercise.model.db
 
+import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import org.jetbrains.anko.db.MapRowParser
 import org.jetbrains.anko.db.SqlOrderDirection
 import org.jetbrains.anko.db.select
+import yeungkc.com.gankio_for_android_proficiency_exercise.App
 import yeungkc.com.gankio_for_android_proficiency_exercise.model.bean.GankResult
 
-class GankDb private constructor(val gankDbHelper: GankDbHelper = GankDbHelper.instance,
-                                 val gankDataMapper: IDataMapper<GankResult> = GankDataMapper) {
+class GankDb constructor(val dbHelper: GankDbHelper = GankDbHelper.instance,
+                         val dataMapper: IDataMapper<GankResult> = GankDataMapper) {
+
+    constructor(context: Context = App.context) : this(GankDbHelper(context), GankDataMapper)
+
     companion object {
         val instance by lazy { GankDb() }
     }
 
     fun saveGank(dataSet: List<GankResult>) {
-        gankDbHelper.use {
+        dbHelper.use {
             dataSet.forEach {
-                insertWithOnConflict(GankTable.NAME, null, gankDataMapper.convertToDomain(it),
+                insertWithOnConflict(GankTable.NAME, null, dataMapper.convertToDomain(it),
                         SQLiteDatabase.CONFLICT_REPLACE)
             }
         }
     }
 
     fun getDataSet(categorical: String): List<GankResult> {
-        return gankDbHelper.use {
+        return dbHelper.use {
             select(GankTable.NAME)
                     .whereSimple("${GankTable.TYPE} = ?", categorical)
                     .orderBy(GankTable.PUBLISHED_AT, SqlOrderDirection.DESC)
                     .parseList(object : MapRowParser<GankResult> {
                         override fun parseRow(columns: Map<String, Any?>) =
                                 columns.run {
-                                    gankDataMapper.convertFromDomain(this)
+                                    dataMapper.convertFromDomain(this)
                                 }
                     })
         }
